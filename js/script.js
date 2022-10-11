e59c4f3f86b57599dc61e968c2f7e56db209c086
Eugenio2104 <109421031+Eugenio2104@users.noreply.github.com>
2022-10-11 17:51:06 +0200

    inserimento document

--- js/script.js
+++ js/script.js
@@ -8,12 +8,30 @@ const nomeCognome = prompt("Inserisci nome e cognome");
 const kmDaPercorrere = parseInt(prompt("Quanti km vuoi percorrere?"));
 const etaPasseggero = parseInt(prompt("Quanti anni hai?")); 
 const prezzoKm = 0.21;
-const scontoMin = 0.20;
-const scontoOver65 = 0.40;
+let sconto = 0;
 const prezzoBiglietto = (kmDaPercorrere * prezzoKm)
 let scontoApplicato;
 let prezzoScontato;
 let prezzoFinale;
+let output;
 
-if(etaPasseggero >= 18 && etaPasseggero <= 65);
+prezzoFinale = prezzoBiglietto;
 
+if(etaPasseggero < 18){
+  sconto = 0.2;
+  prezzoFinale = prezzoBiglietto - (prezzoBiglietto * sconto);
+}
+
+if(etaPasseggero > 65) {
+  sconto = 0.4;
+    prezzoFinale = prezzoBiglietto - (prezzoBiglietto * sconto);
+}
+
+console.log('Il prezzo del biglietto è ',prezzoFinale)
+
+
+document.getElementById('prezzo-biglietto').innerHTML= `prezzo biglietto`
+output = `
+${prezzoFinale}
+`;
+document.getElementById('prezzo-biglietto').innerHTML += output;
